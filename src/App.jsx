220aef756d775a1ddf687e483d95e9a90da503a9
Eric Li <eric.li.8736@gmail.com>
2023-03-25 16:04:54 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import wordService from "./services/words";
+import parse from "html-react-parser";
+
+const App = () => {
+  const [words, setWords] = useState([]);
+  const [selectedWord, setSelectedWord] = useState(null);
+
+  useEffect(() => {
+    wordService.getAll().then((initialWords) => {
+      setWords(initialWords);
+      console.log(initialWords[0]);
+    });
+  }, []);
+
+  const getRandomId = () => {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    const temp = words[randomIndex];
+    temp.word = trimWord(temp.word);
+    temp.definition = trimWord(temp.definition);
+    temp.etymology = trimWord(temp.etymology);
+    return temp;
+  };
+
+  const trimWord = (entry) => {
+    return entry.substring(2, entry.length - 2);
+  };
+
+  const handleNextWord = () => {
+    setSelectedWord(getRandomId());
+    if (selectedWord) {
+      console.log(typeof selectedWord.etymology);
+    }
+  };
+
+  const handleEtymology = () => {
+    if (selectedWord) {
+      return parse(selectedWord.etymology);
+    }
+    return null;
+  };
+
+  return (
+    <div>
+      <h1>Random Word</h1>
+      <button onClick={handleNextWord}>Next Word</button>
+      <div className="word">
+        <p>{selectedWord?.word}</p>
+        <p>{selectedWord?.definition}</p>
+        {handleEtymology()}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/services/words.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+const baseUrl = 'http://localhost:3001/words';
+
+const getAll = () => {
+    const persons = axios.get(baseUrl)
+    return persons.then(response => response.data)
+}
+  
+const create = newObject => {
+    const person = axios.post(baseUrl, newObject)
+    return person.then(response => response.data)
+}
+
+const remove = id => {
+    const person = axios.delete(`${baseUrl}/${id}`);
+    return person.then(response => response.data);
+}
+
+const update = (id, newObject) => {
+    const person = axios.put(`${baseUrl}/${id}`, newObject);
+    return person.then(response => response.data);
+}
+
+export default {getAll, create, remove, update}
