76bb4a21765ca758510e76b7ee50eee0ef0ffc29
Eric Li <eric.li.8736@gmail.com>
2023-09-28 22:49:10 -0500

    Add react context

--- src/components/WordDisplay.jsx
+++ src/components/WordDisplay.jsx
@@ -1,22 +1,26 @@
 import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
-import wordService from '../services/words'
+import wordService from "../services/words";
 
 const WordDisplay = () => {
-    const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
-    const words = useQuery({queryKey: ['words'], queryFn: wordService.getAll})
+  const words = useQuery({
+    queryKey: ["words"],
+    queryFn: wordService.getAll,
+    refetchOnWindowFocus: false,
+    refetchOnmount: false,
+    staleTime: 1000 * 60 * 5,
+  });
 
-    if(words.isLoading)
-    {
-        return (
-            <div>
-                fetching words
-            </div>
-        )
-    }
+  if (words.isLoading) {
+    return <div>fetching words</div>;
+  }
 
-    console.log(words.data)
+  if (words.isError) {
+    return <div>error</div>;
+  }
 
+  console.log(words.data[0]);
 };
 
 export default WordDisplay;
---
+++ src/contexts/DateContext.jsx
@@ -0,0 +1,47 @@
+import { createContext, useReducer, useContext } from "react";
+
+const changeDate = (date, change) => {
+  const subtract = 24 * 60 * 60 * 1000;
+  if (
+    date < new Date().setTime(new Date().getTime() - subtract) &&
+    change === 1
+  )
+    return (new Date(date.setDate(date.getDate() + change)));
+  else if (date > new Date(2023, 2, 27) && change === -1)
+    return (new Date(date.setDate(date.getDate() + change)));
+};
+
+const dateReducer = ( state = new Date(), action ) => {
+  switch (action.type) {
+    case "INC":
+      return changeDate(state, 1)
+    case "DEC":
+      return changeDate(state, -1)
+    case "ZERO":
+      return new Date()
+  }
+}
+
+const DateContext = createContext()
+
+export const DateContextProvider = (props) => {
+  const [date, dateDispatch] = useReducer(counterReducer, 0)
+
+  return (
+    <DateContext.Provider value={[date, dateDispatch] }>
+      {props.children}
+    </DateContext.Provider>
+  )
+}
+
+export const useDateValue = () => {
+  const dateAndDispatch = useContext(DateContext)
+  return dateAndDispatch[0]
+}
+
+export const useCounterDispatch = () => {
+  const dateAndDispatch = useContext(DateContext)
+  return dateAndDispatch[1]
+}
+
+export default CounterContext
